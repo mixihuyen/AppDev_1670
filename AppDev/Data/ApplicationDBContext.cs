6f902f0cfff01e8f92e0199911c5850cbaebe851
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-11-23 10:26:14 +0700

    AddMigrations

--- AppDev/Data/ApplicationDBContext.cs
+++ AppDev/Data/ApplicationDBContext.cs
@@ -4,9 +4,21 @@ namespace AppDev.Data
 {
     public class ApplicationDBContext : DbContext
     {
+        DbSet<Category> Category {  get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base (options)
         {
 
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name = "Horror", Description ="So scary", DisplayOrder = 2},
+                new Category { Id = 2, Name = "Action", Description = "Hello", DisplayOrder = 3 },
+                new Category { Id = 3, Name = "Ronman", Description = " Hi", DisplayOrder = 4 },
+                new Category { Id = 4, Name = "Science", Description = " So", DisplayOrder = 5 }
+                );
         }
     }
 }
---
+++ AppDev/Migrations/20231123032423_AddSeedingCategoryToTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AppDev.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedingCategoryToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Description", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, "So scary", 2, "Horror" },
+                    { 2, "Hello", 3, "Action" },
+                    { 3, " Hi", 4, "Ronman" },
+                    { 4, " So", 5, "Science" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
