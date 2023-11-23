9d324bab14db041132e89a18b2079359f3006f30
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-11-23 09:23:16 +0700

    Database

---
+++ AppDev/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppDev.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base (options)
+        {
+
+        }
+    }
+}
