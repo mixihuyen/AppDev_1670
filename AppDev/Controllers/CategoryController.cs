e5c52c9eb98974f8acd2c3910137aec531f47995
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-11-25 14:53:51 +0700

    change

--- AppDev/Controllers/CategoryController.cs
+++ AppDev/Controllers/CategoryController.cs
@@ -27,15 +27,64 @@ namespace AppDev.Controllers
 		[HttpPost]
 		public IActionResult Create(Category category)
 		{
-
+			if (category.Name == category.Description) 
+			{
+				ModelState.AddModelError("Description", "Name can not be equal to Description");
+			}
 			if (ModelState.IsValid)
 			{
 				_dbContext.Categories.Add(category);
 				_dbContext.SaveChanges();
 				TempData["success"] = "Category Created successfully";
+                return RedirectToAction("Index");
+            }
+			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0) 
+			{
+				return NotFound();
 			}
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+		{
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category update successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{
+			_dbContext.Categories.Remove(category);
+			_dbContext.SaveChanges();
+			TempData["success"] = "Category Deleted successfully";
 			return RedirectToAction("Index");
-
 		}
 	}
 }
--- AppDev/Models/Category.cs
+++ AppDev/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppDev.Models
 {
@@ -9,6 +10,8 @@ namespace AppDev.Models
 		[Required]
 		public string Name { get; set; }
 		public string Description { get; set; }
+		[DisplayName ("Display Order of Category")]
+		[Range (1,100)]
 		public int DisplayOrder { get; set; }
 	}
 }
