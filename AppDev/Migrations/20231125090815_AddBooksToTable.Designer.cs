c8918eb78110da1d6c9d18ec68512c6a267e09ac
Le Thi Thanh Huyen <lehuyen23vn@gmail.com>
2023-11-25 16:44:19 +0700

    change

--- AppDev/Migrations/20231125090815_AddBooksToTable.Designer.cs
+++ AppDev/Migrations/20231125090815_AddBooksToTable.Designer.cs
@@ -3,6 +3,7 @@ using AppDev.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppDev.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231125090815_AddBooksToTable")]
+    partial class AddBooksToTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
